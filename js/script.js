b4bb88754dea33c25ab1e1e9ab8ae53399f60b29
irfanfldi <irfannfldi@gmail.com>
2025-11-20 20:39:10 +0700

    nambahin js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+welcomeMessage();
+
+
+function welcomeMessage() {
+    let userName = prompt("Please enter your name:");
+    if (userName === '' || userName=== null) {
+        userName = "Guest";
+    }
+    
+    document.getElementById("welcome-speech").innerText = "Welcome, " + userName + "!";
+    
+}
